f4eaa7311a6097981f5c386f6100c25f6b97d712
floweryk <flowerk94@gmail.com>
2022-02-25 09:43:24 +0900

    [refactor] utils 삭제

--- get_norm_parameter.py
+++ get_norm_parameter.py
@@ -2,9 +2,9 @@ import os
 import cv2
 import numpy as np
 from config import CLASSES
-from utils import save, load
 
-def save_total_data(data_dir):
+
+def calculate_mean_var(data_dir):
     total_data = []
 
     for class_name in CLASSES.keys():
@@ -17,14 +17,14 @@ def save_total_data(data_dir):
             img = cv2.imread(file_path)
             total_data.append(img)
     
-    save(total_data, "total_data.list")
     
+    # calculate statistics
+    total_data = np.array(total_data)
+    print(total_data.shape)                     # (50000, 32, 32, 3)
+    print(np.mean(total_data, axis=(0, 1, 2)))  # [113.86283869 122.93301916 125.26884795]
+    print(np.std(total_data, axis=(0, 1, 2)))   # [66.46033586 61.90929531 62.5340599 ]
 
-# save_total_data(os.path.join('data', 'train'))
-total_data = load("total_data.list")
-total_data = np.array(total_data)
 
-# calculate statistics
-print(total_data.shape)                     # (50000, 32, 32, 3)
-print(np.mean(total_data, axis=(0, 1, 2)))  # [113.86283869 122.93301916 125.26884795]
-print(np.std(total_data, axis=(0, 1, 2)))   # [66.46033586 61.90929531 62.5340599 ]
+if __name__ == "__main__":
+    dir = os.path.join("data", "train")
+    calculate_mean_var(dir)
