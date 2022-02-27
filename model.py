615444236282c6b5b193f863264c45a8a983f198
Minsang Yu <flowerk94@gmail.com>
2022-02-27 17:22:24 +0900

    [feat] 깊은 CNN 모델 구현

--- model.py
+++ model.py
@@ -4,20 +4,49 @@ class Model(nn.Module):
     def __init__(self):
         super().__init__()
         self.layer1 = nn.Sequential(
-            # nn.Flatten(start_dim=1),    # to reshape with considering batch size
-            # nn.Linear(32*32*3, 10),
-            # nn.Softmax(dim=0)           # nn.CrossEntropyLoss already includes softmax
-            nn.Conv2d(3, 64, kernel_size=3),
-            nn.BatchNorm2d(64),
+            # CNN Block 
+            nn.Conv2d(3, 256, kernel_size=3, padding=1),
             nn.LeakyReLU(),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Dropout2d(p=0.35),
 
-            nn.Flatten(start_dim=1),
-            nn.Linear(57600, 10)
+            # CNN Block 
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(128),
+            # nn.MaxPool2d(kernel_size=2),
+            nn.Dropout2d(p=0.35),
+
+            # CNN Block 
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(64),
+            # nn.MaxPool2d(kernel_size=2),
+            nn.Dropout2d(p=0.35),
+
+            # Flatten
+            nn.Flatten(start_dim=1),    # to reshape with considering batch size
+            
+            # Linear Block
+            nn.Linear(16384, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+
+            # Linear Block
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            
+            # Output Block
+            nn.Linear(256, 10),
+            # nn.Softmax(dim=0)           # nn.CrossEntropyLoss already includes softmax
         )
     
     def forward(self, x):
         x = x.float()
         x = self.layer1(x)
+        
         return x
 
 
