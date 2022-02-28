45be205655a1593bc6beeca9ff29bdbe603dc002
Minsang Yu <flowerk94@gmail.com>
2022-02-28 22:58:22 +0900

    [feat] data augmentation

--- config.py
+++ config.py
@@ -4,14 +4,17 @@ import torch
 class DatasetConfig:
     data_dir = os.path.join("data", "train")
     classes = {"airplane": 0, "automobile": 1, "bird": 2, "cat": 3, "deer": 4, "dog": 5, "frog": 6, "horse": 7, "ship": 8, "truck": 9}
+    aug_ratio = 2
     is_norm = True
-    norm_mean = [113.86283869, 122.93301916, 125.26884795]
-    norm_std = [66.46033586, 61.90929531, 62.5340599]
+    norm_mean = [0., 0., 0.]
+    norm_std = [255., 255., 255.]
+    # norm_mean = [113.86283869, 122.93301916, 125.26884795]
+    # norm_std = [66.46033586, 61.90929531, 62.5340599]
 
 class TrainerConfig:
     device = "cuda" if torch.cuda.is_available() else 'cpu'
-    lr = 3e-4
-    weight_decay = 0.1
+    lr = 1e-4
+    weight_decay = 0.05
     batch_size = 64
     num_workers = 0
     num_epochs = 100
--- dataset.py
+++ dataset.py
@@ -3,6 +3,29 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 from torch.utils.data import Dataset
+from torchvision import transforms as T
+
+
+def t(img):
+    random_choice = T.RandomChoice([
+        T.RandomResizedCrop((32, 32)),
+        T.RandomHorizontalFlip(p=1),
+        T.RandomVerticalFlip(p=1),
+        T.RandomAffine(degrees=30, translate=(0.2, 0.2)),
+        T.ColorJitter(brightness=0.05, contrast=0.05),
+        T.RandomGrayscale(p=1),
+    ])
+
+    transform = T.Compose([
+        T.ToPILImage(),
+        random_choice, 
+        random_choice, 
+        random_choice, 
+        random_choice, 
+        lambda x: np.array(x)
+    ])
+    return transform(img)
+
 
 class ObjectDataset(Dataset):
     def __init__(self, config):
@@ -21,20 +44,24 @@ class ObjectDataset(Dataset):
                     file_path = os.path.join(class_dir, file_name)
 
                     # read img and change from int to float
-                    img = cv2.imread(file_path)
-                    img = img.astype(np.float64)
+                    img_org = cv2.imread(file_path)
 
-                    # (optional) normalize
-                    if config.is_norm:
-                        img -= np.array(config.norm_mean)
-                        img *= 1 / np.array(config.norm_std)
-                    
-                    # (optional) roll axis
-                    img = np.moveaxis(img, -1, 0)
+                    for _ in range(config.aug_ratio):
+                        # (optional) data augmentation
+                        img = t(img_org) if config.aug_ratio > 1 else img_org
 
-                    # save img to dataset
-                    self.data[i] = (img, target)
-                    i += 1
+                        # (optional) normalize
+                        img = img.astype(np.float64)
+                        if config.is_norm:
+                            img -= np.array(config.norm_mean)
+                            img *= 1 / np.array(config.norm_std)
+                        
+                        # (optional) roll axis
+                        img = np.moveaxis(img, -1, 0)
+
+                        # save img to dataset
+                        self.data[i] = (img, target)
+                        i += 1
         
         print("loading data from:", config.data_dir)
         load_data()
